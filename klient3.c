85065c6d244f4af80aa049b8278367f77dfd1628
Beretus <48188253+Beretus@users.noreply.github.com>
2024-01-01 20:47:36 +0100

    Add files via upload

---
+++ klient3.c
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <sys/time.h>
+#include <errno.h>
+#include <time.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int sockfd;
+struct sockaddr_in servaddr;
+char username[50];
+volatile sig_atomic_t sigflag = 0;
+
+void send_request(int sockfd, struct sockaddr_in servaddr, char* username, char* password, char* action);
+void handle_sigint(int sig);
+void send_message(int sockfd, struct sockaddr_in servaddr, char* username, char* message, char* action);
+
+void handle_sigint(int sig) {
+    sigflag = 1;
+}
+
+void send_request(int sockfd, struct sockaddr_in servaddr, char* username, char* password, char* action) {
+    char full_message[BUFFER_SIZE];
+    sprintf(full_message, "%s:%s:%s", action, username, password);
+    sendto(sockfd, (const char*)full_message, strlen(full_message), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+}
+
+void send_message(int sockfd, struct sockaddr_in servaddr, char* username, char* message, char* action) {
+    char full_message[BUFFER_SIZE];
+    sprintf(full_message, "%s:%s:%s", action, username, message);
+    int bytes_sent = sendto(sockfd, (const char*)full_message, strlen(full_message), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
+    if (bytes_sent < 0) {
+        perror("Błąd wysyłania wiadomości");
+    } else {
+        //printf("Wysłano %d bajtów\n", bytes_sent);
+    }
+}
+
+int main() {
+    struct sockaddr_in servaddr;
+    fd_set readfds;
+    int max_sd, activity, valread;
+    char buffer[BUFFER_SIZE];
+    char password[50];
+    int flags;
+    int logged_in = 0;
+    struct timeval last_heartbeat;
+    time_t server_activity;
+    gettimeofday(&last_heartbeat, NULL);
+    server_activity = time(NULL);
+
+    struct timeval now;
+
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    flags = fcntl(sockfd, F_GETFL, 0);
+    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    max_sd = sockfd;
+
+    do {
+        printf("Podaj swoją nazwę użytkownika:");
+        fgets(username, sizeof(username), stdin);
+        username[strcspn(username, "\n")] = 0; 
+        if(strlen(username) == 0) {
+            printf("Niepoprawna nazwa użytkownika, nie może być pusta\n");
+        }
+    } while(strlen(username) == 0); 
+    username[strcspn(username, "\n")] = 0;
+    printf("EXIT - Wyjscie\nWybierz opcję:\n1. Logowanie\n2. Rejestracja\n3. EXIT\n");
+    signal(SIGINT, handle_sigint);
+
+    while (1) {
+        if (sigflag) {
+            char exit_message[BUFFER_SIZE] = "EXIT";
+            send_message(sockfd, servaddr, username, exit_message, "EXIT");
+            close(sockfd);
+            exit(0);
+        }
+
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+
+        struct timeval timeout;
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, &timeout);
+
+        if ((activity < 0) && (errno != EINTR)) {
+            printf("select error");
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+            if (!logged_in) {
+                int choice;
+
+                printf("Wybierz opcję:\n1. Logowanie\n2. Rejestracja\n3. EXIT\n");
+                scanf("%d", &choice);
+                getchar(); 
+
+                if (choice == 3) {
+                    exit(0); 
+                } else if (choice == 1 || choice == 2) {
+                    do {
+                        printf("Podaj hasło:");
+                        fgets(password, sizeof(password), stdin);
+                        password[strcspn(password, "\n")] = 0; 
+                        if(strlen(password) == 0) {
+                            printf("**Niepoprawne hasło, nie może być puste**\n");
+                        }
+                    } while(strlen(password) == 0);
+
+                    if (choice == 1) {
+                        send_request(sockfd, servaddr, username, password, "LOGIN");
+                        // gettimeofday(&now, NULL);
+			        // send_message(sockfd, servaddr, username, "HEARTBEAT", "HEARTBEAT");
+                    // last_heartbeat = now;
+			            
+                    } else if (choice == 2) {
+                        send_request(sockfd, servaddr, username, password, "REGISTER");
+                        // gettimeofday(&now, NULL);
+			        // send_message(sockfd, servaddr, username, "HEARTBEAT", "HEARTBEAT");
+                    // last_heartbeat = now;
+			            
+                    }
+                } else {
+                    printf("**Nieprawidłowa opcja!**\n");
+                }
+            } else {
+                
+                // printf("> ");
+                fgets(buffer, BUFFER_SIZE, stdin);
+                if(time(NULL) - server_activity > 7) {
+                    printf("SERWER NIE ODPOWIADA!");
+                }
+                buffer[strcspn(buffer, "\n")] = 0; 
+
+                if (strcmp(buffer, "EXIT") == 0) {
+                    send_message(sockfd, servaddr, username, buffer, "EXIT");
+                    exit(0);
+                } else {
+                    if (strlen(buffer) > 0) {
+                        send_message(sockfd, servaddr, username, buffer, "MESSAGE");
+                    }
+                }
+                
+            }
+        }
+	struct timeval now;
+	gettimeofday(&now, NULL);
+	if (now.tv_sec - last_heartbeat.tv_sec >= 5) {
+            send_message(sockfd, servaddr, username, "HEARTBEAT", "HEARTBEAT");
+            last_heartbeat = now;
+        }
+            signal(SIGINT, handle_sigint);
+
+       
+        if (FD_ISSET(sockfd, &readfds)) {
+            socklen_t len = sizeof(servaddr); 
+            valread = recvfrom(sockfd, (char*)buffer, BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr*)&servaddr, &len);
+
+            if (valread > 0) {
+                buffer[valread] = '\0';
+                if(strcmp(buffer, "HEARTBEAT_ACK") == 0) {
+                    server_activity = time(NULL);
+                } else {
+                    printf("%s\n", buffer);
+                }
+                
+                
+                if (strcmp(buffer, "LOGIN_SUCCESS") == 0 || strcmp(buffer, "REGISTER_SUCCESS") == 0) {
+                    logged_in = 1;
+                    // gettimeofday(&now, NULL);
+			        // send_message(sockfd, servaddr, username, "HEARTBEAT", "HEARTBEAT");
+                    // last_heartbeat = now;
+                }
+            }
+        }
+    }
+
+    close(sockfd);
+    return 0;
+}
---
+++ server3.c
@@ -0,0 +1,284 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <syslog.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define MAX_CLIENTS 3
+#define USER_FILE "users.txt"
+
+struct user_info {
+    char username[50];
+    struct sockaddr_in address;
+    socklen_t addr_len;
+    int active;
+    int logged_in;
+    time_t last_heartbeat;
+};
+
+struct user_info clients[MAX_CLIENTS];
+
+int find_client(struct sockaddr_in *cli_addr) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active && memcmp(cli_addr, &clients[i].address, sizeof(struct sockaddr_in)) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int check_existing_user(struct user_info users[], int num_users, char *new_username) {
+    for (int i = 0; i < num_users; i++) {
+        if (users[i].logged_in == 1 && strcmp(users[i].username, new_username) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int add_client(struct sockaddr_in *cli_addr, socklen_t addr_len, char *username) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (!clients[i].active) {
+            strncpy(clients[i].username, username, sizeof(clients[i].username) - 1);
+            clients[i].address = *cli_addr;
+            clients[i].addr_len = addr_len;
+            clients[i].active = 1;
+            clients[i].logged_in = 0;
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+void broadcast_message(int sockfd, char *username, char *message, struct user_info *clients, int sender_index) {
+    char full_message[BUFFER_SIZE];
+    sprintf(full_message, "%s: %s", username, message);
+    printf("%s\n", full_message);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active && clients[i].logged_in && i != sender_index) {
+            sendto(sockfd, full_message, strlen(full_message), 0, (const struct sockaddr *)&clients[i].address, clients[i].addr_len);
+        }
+    }
+}
+
+void broadcast_log(int sockfd, char *message, struct user_info *clients, int sender_index) {
+    char full_message[BUFFER_SIZE];
+    sprintf(full_message, "%s", message);
+    printf("%s\n", full_message);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].active && clients[i].logged_in && i != sender_index) {
+            sendto(sockfd, full_message, strlen(full_message), 0, (const struct sockaddr *)&clients[i].address, clients[i].addr_len);
+        }
+    }
+}
+
+int check_credentials(char *username, char *password) {
+    FILE *file = fopen(USER_FILE, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 0;
+    }
+
+    char line[BUFFER_SIZE];
+    while (fgets(line, sizeof(line), file)) {
+        char *file_username = strtok(line, ":");
+        char *file_password = strtok(NULL, "\n");
+        if (strcmp(username, file_username) == 0 && strcmp(password, file_password) == 0) {
+            fclose(file);
+            syslog(LOG_LOCAL1 | LOG_INFO, "Udana proba logowania: %s", username);
+            return 1; //login udany
+        }
+    }
+    fclose(file);
+    syslog(LOG_LOCAL1 | LOG_ERR, "Nieudana proba logowania: %s", username);
+    return 0; // login nieudany
+}
+
+int register_user(char *username, char *password) {
+    FILE *file = fopen(USER_FILE, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 0;
+    }
+
+    char line[BUFFER_SIZE];
+    while (fgets(line, sizeof(line), file)) {
+        char *file_username = strtok(line, ":");
+        if (strcmp(username, file_username) == 0) {
+            fclose(file);
+            syslog(LOG_LOCAL1 | LOG_ERR, "Proba rejestracji istniejacego uzytkownika! %s", username);
+            return 0; // user istnieje
+        }
+    }
+    fclose(file);
+
+    file = fopen(USER_FILE, "a");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 0;
+    }
+    fprintf(file, "%s:%s\n", username, password);
+    fclose(file);
+    syslog(LOG_LOCAL1 | LOG_INFO, "Uzytkownik %s zarejestrowany", username);
+    return 1; // rejestracja udana
+}
+
+void handle_login(int sockfd, struct sockaddr_in *cli_addr, socklen_t addr_len, char *username, char *password) {
+    if (check_credentials(username, password)) {
+        int client_index = find_client(cli_addr);
+        if (client_index == -1) {
+            client_index = add_client(cli_addr, addr_len, username);
+        }
+        if (client_index != -1) {
+            int user_exists = check_existing_user(clients, 3, username);
+            if(user_exists) {
+                char response[] = "Uzytkownik o takiej nazwie jest juz zalogowany";
+            sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)cli_addr, addr_len); 
+            
+            } else {
+               clients[client_index].logged_in = 1;
+	       clients[client_index].last_heartbeat = time(NULL);
+            char response[] = "LOGIN_SUCCESS";
+            sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)cli_addr, addr_len);
+            }
+            
+        }
+    } else {
+        char response[] = "Niepoprawna proba logowania!";
+        sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)cli_addr, addr_len);
+    }
+}
+
+void handle_register(int sockfd, struct sockaddr_in *cli_addr, socklen_t addr_len, char *username, char *password) {
+    if (register_user(username, password)) {
+        int client_index = add_client(cli_addr, addr_len, username);
+        if (client_index != -1) {
+            clients[client_index].logged_in = 1;
+	    clients[client_index].last_heartbeat = time(NULL);
+            char response[] = "REGISTER_SUCCESS";
+            syslog(LOG_LOCAL1 | LOG_INFO, "Udana rejestracja %s", username);
+            sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)cli_addr, addr_len);
+
+        }
+    } else {
+        char response[] = "Rejestracja nieudana!";
+        syslog(LOG_LOCAL1 | LOG_ERR, "Nieudana rejestracja %s", username);
+        sendto(sockfd, response, strlen(response), 0, (const struct sockaddr *)cli_addr, addr_len);
+    }
+}
+
+int main() {
+    openlog("server3", LOG_PID, LOG_DAEMON);
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+    fd_set readfds;
+    int max_sd, activity, valread;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("UDP Server Listening on port %d\n", PORT);
+    max_sd = sockfd;
+
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        struct timeval tv;
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+
+        activity = select(max_sd + 1, &readfds, NULL, NULL, &tv);
+        if (activity > 0 && FD_ISSET(sockfd, &readfds)) {
+            char buffer[BUFFER_SIZE];
+            socklen_t len = sizeof(cliaddr);
+            valread = recvfrom(sockfd, (char *)buffer, BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr *)&cliaddr, &len);
+
+            if (valread > 0) {
+                buffer[valread] = '\0';
+                char *action = strtok(buffer, ":");
+                char *username = strtok(NULL, ":");
+                char *password = strtok(NULL, "\n");
+
+                if (strcmp(action, "LOGIN") == 0) {
+                    handle_login(sockfd, &cliaddr, len, username, password);
+		            int client_index = find_client(&cliaddr);
+                    if (client_index != -1 && clients[client_index].logged_in) {
+                        char annoucement[BUFFER_SIZE];
+                        sprintf(annoucement, "Uzytkownik %s dolaczyl do forum!", username);
+                        syslog(LOG_LOCAL1 | LOG_INFO, "UWAGA: %s", annoucement);
+                        broadcast_log(sockfd, annoucement, clients, client_index);
+                        }
+                } else if (strcmp(action, "REGISTER") == 0) {
+
+                    handle_register(sockfd, &cliaddr, len, username, password);
+		            int client_index = find_client(&cliaddr);
+                    if (client_index != -1 && clients[client_index].logged_in) {
+                        char annoucement[BUFFER_SIZE];
+                        sprintf(annoucement, "Uzytkownik %s zarejestrowal sie do forum!", username);
+                        syslog(LOG_LOCAL1 | LOG_INFO, "UWAGA: %s", annoucement);
+                        broadcast_log(sockfd, annoucement, clients, client_index);
+                    }
+                }else if (strcmp(action, "EXIT") == 0) {
+                    int client_index = find_client(&cliaddr);
+                    if (client_index != -1 && clients[client_index].logged_in) {
+                        char annoucement[BUFFER_SIZE];
+                        sprintf(annoucement, "Uzytkownik %s opuscil forum!", username);
+                        syslog(LOG_LOCAL1 | LOG_INFO, "UWAGA: %s", annoucement);
+                        broadcast_log(sockfd, annoucement, clients, client_index);
+                        clients[client_index].active = 0;
+                        clients[client_index].logged_in = 0;
+                        strcpy(clients[client_index].username, "");
+                    }
+                }
+		else if (strcmp(action, "HEARTBEAT") == 0) {
+                int client_index = find_client(&cliaddr);
+                if (client_index != -1) {
+                    clients[client_index].last_heartbeat = time(NULL);
+                    char *message = "HEARTBEAT_ACK";
+                    sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+		            printf("Otrzymano HEARTBEAT od klienta: %s\n", clients[client_index].username);
+                }
+		}
+                else {
+			        int client_index = find_client(&cliaddr);
+                    if (client_index != -1 && clients[client_index].logged_in) {
+                    broadcast_message(sockfd, clients[client_index].username, password, clients, client_index);
+		            }   
+            }
+        }
+    }
+	for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].logged_in && time(NULL) - clients[i].last_heartbeat > 8) {
+            printf("Klient %s stracił połączenie\n", clients[i].username);
+            syslog(LOG_LOCAL1 | LOG_INFO, "Klient %s utracil polaczenie.", clients[i].username);
+            clients[i].active = 0;
+            strcpy(clients[i].username, "");
+        }
+    }
+}
+    closelog();
+    return 0;
+}
+
